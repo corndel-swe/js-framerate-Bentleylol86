23b325f0be1fcd42a7d30bfe303362da9837fc8c
Bentleylol86 <Laurence.Woodhouse@bentley.co.uk>
2025-01-28 12:09:23 +0000

    another sorry attempt day 2 last task done the lord has blessed me

--- exercises/d2e3.js
+++ exercises/d2e3.js
@@ -1,20 +1,23 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
-app.set('view engine', 'ejs')
-app.set('views', 'exercises/views')
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'exercises/views');
 
-// TODO: configure the app to use urlencoded
+// Configure the app to use URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/d2e3', (req, res) => {
-  res.render('d2e3')
-})
+  res.render('d2e3');
+});
 
 app.post('/submit', (req, res) => {
-  // TODO: get the `name` and `email` from the form
-  // so that the below response works
+  // Extract the `name` and `email` from the form
+const name = req.body.name;
+const email = req.body.email;
 
-  res.send(`Received: ${name}, ${email}`)
-})
 
-export default app
+  res.send(`Received: ${name}, ${email}`);
+});
+
+export default app;
